be4482727cf373ba2e458e56a010956ceb62745b
Daniel Jerusalmy <dannyerushalmi@gmail.com>
2022-04-09 02:46:16 -0400

    text

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -77,16 +77,19 @@ const Work = () => {
 								}}
 								className="app__work-hover app__flex"
 							>
-								<a href={work.projectLink} target="_blank" rel="noreferrer">
-									<motion.div
-										whileInView={{ scale: [0, 1] }}
-										whileHover={{ scale: [1, 0.9] }}
-										transition={{ duration: 0.25 }}
-										className="app__flex"
-									>
-										<AiFillEye />
-									</motion.div>
-								</a>
+								{!work.tags.includes("Mobile") && (
+									<a href={work.projectLink} target="_blank" rel="noreferrer">
+										<motion.div
+											whileInView={{ scale: [0, 1] }}
+											whileHover={{ scale: [1, 0.9] }}
+											transition={{ duration: 0.25 }}
+											className="app__flex"
+										>
+											<AiFillEye />
+										</motion.div>
+									</a>
+								)}
+
 								<a href={work.codeLink} target="_blank" rel="noreferrer">
 									<motion.div
 										whileInView={{ scale: [0, 1] }}
@@ -94,7 +97,7 @@ const Work = () => {
 										transition={{ duration: 0.25 }}
 										className="app__flex"
 									>
-										{!work.tags.includes("Mobile") && <AiFillGithub />}
+										<AiFillGithub />
 									</motion.div>
 								</a>
 							</motion.div>
