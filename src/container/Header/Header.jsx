9371bb7d90760a1f3bf4d89c3353cbedaca39b25
Daniel Jerusalmy <dannyerushalmi@gmail.com>
2022-04-09 02:09:46 -0400

    text

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => (
 
 				<div className="tag-cmp app__flex">
 					<p className="p-text">Web Developer</p>
-					<p className="p-text">Freelancer</p>
+					<p className="p-text">Mobile Developer</p>
 				</div>
 				<div className="tag-cmp app__flex hiddenMobile">
 					<p className="p-text">Search Engine Optimisation</p>
