77cd73966ba6a819d721a897bbcd6a29279709a4
Daniel Jerusalmy <dannyerushalmi@gmail.com>
2022-04-09 02:37:50 -0400

    text

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -43,7 +43,7 @@ const Work = () => {
 			</h2>
 
 			<div className="app__work-filter">
-				{["FullStack", "Web 3.0", "Frontend", "React JS", "All"].map(
+				{["FullStack", "Mobile", "Frontend", "React JS", "All"].map(
 					(item, index) => (
 						<div
 							key={index}
