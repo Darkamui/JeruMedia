25d3e62f8102fe709fb4652154916c47569a9f7d
Daniel Jerusalmy <dannyerushalmi@gmail.com>
2022-04-09 02:39:54 -0400

    text

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -49,14 +49,14 @@ const Footer = () => {
 			<div className="app__footer-cards">
 				<div className="app__footer-card ">
 					<img src={images.email} alt="email" />
-					<a href="mailto:hello@micael.com" className="p-text">
-						hello@jerumedia.ca
+					<a href="mailto:dannyerushalmi@gmail.com" className="p-text">
+						dannyerushalmi@gmail.com
 					</a>
 				</div>
 				<div className="app__footer-card">
 					<img src={images.mobile} alt="phone" />
-					<a href="tel:+1 (123) 456-7890" className="p-text">
-						+1 (418) 350-4649
+					<a href="tel:+1 (418) 350-****" className="p-text">
+						+1 (418) 350-****
 					</a>
 				</div>
 			</div>
