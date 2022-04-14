3c4fe0e30cb194121659f48d42dfeede49fa0ecc
Daniel Jerusalmy <dannyerushalmi@gmail.com>
2022-04-14 19:24:42 -0400

    redesign

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub, AiOutlineDoubleRight } from "react-icons/ai";
+import { BiLinkExternal } from "react-icons/bi";
 import { motion } from "framer-motion";
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
@@ -62,74 +63,99 @@ const Work = () => {
 				className="app__work-portfolio"
 			>
 				{filterWork.map((work, index) => (
-					<div className="app__work-item app__flex" key={index}>
-						<div className="app__work-img app__flex">
-							<img src={urlFor(work.imgUrl)} alt={work.name} />
-
-							<motion.div
-								whileHover={{ opacity: [0, 1] }}
-								transition={{
-									duration: 0.25,
-									ease: "easeInOut",
-									staggerChildren: 0.5,
-								}}
-								className="app__work-hover app__flex"
-							>
-								{!work.tags.includes("Mobile") && (
-									<a href={work.projectLink} target="_blank" rel="noreferrer">
-										<motion.div
-											whileInView={{ scale: [0, 1] }}
-											whileHover={{ scale: [1, 0.9] }}
-											transition={{ duration: 0.25 }}
-											className="app__flex"
-										>
-											<AiFillEye />
-										</motion.div>
+					<div className="appWorkContainer">
+						<div className="bgWorkOverlay"></div>
+						<div className="hrWork"></div>
+						<div className="middleWork">
+							<h4 className="h4Work">{work.title}</h4>
+							<p className="pWork">{work.description}</p>
+							<div className="workActionBtns">
+								<div className="leftActionBtns">
+									<a href="" className="actionBtnProject">
+										view project
+										<i class="fa-solid fa-arrow-up-right-from-square"></i>
 									</a>
-								)}
-
-								<a href={work.codeLink} target="_blank" rel="noreferrer">
-									<motion.div
-										whileInView={{ scale: [0, 1] }}
-										whileHover={{ scale: [1, 0.9] }}
-										transition={{ duration: 0.25 }}
-										className="app__flex"
-									>
-										<AiFillGithub />
-									</motion.div>
-								</a>
-							</motion.div>
-						</div>
-
-						<div className="app__work-content app__flex">
-							<h4 className="bold-text">{work.title}</h4>
-							<p className="p-text" style={{ marginTop: 10 }}>
-								{work.description}
-							</p>
-
-							<div className="app__work-tag app__flex">
-								<p className="p-text">{work.tags[0]}</p>
+									<a href="">
+										github <i class="fa-brands fa-github"></i>
+									</a>
+								</div>
+								<div className="rightActionBtns">
+									{work.tags.map((tag) => (
+										<span>{tag}</span>
+									))}
+								</div>
 							</div>
 						</div>
-						<div className="detailAction">
-							<Link
-								to={{ pathname: `/${work.title}`, state: { work: work } }}
-								className="app__work-btn"
-								state={{ work: work }}
-							>
-								More Details
-							</Link>
-							<Link
-								to={{ pathname: `/${work.title}`, state: { work: work } }}
-								state={{ work: work }}
-							>
-								<AiOutlineDoubleRight
-									size={"1.5rem"}
-									style={{ color: "#7980c9", marginTop: ".5rem" }}
-								/>
-							</Link>
-						</div>
+						<img src={urlFor(work.imgUrl)} alt="" />
 					</div>
+					// <div className="app__work-item app__flex" key={index}>
+					// 	<div className="app__work-img app__flex">
+					// 		<img src={urlFor(work.imgUrl)} alt={work.name} />
+
+					// 		<motion.div
+					// 			whileHover={{ opacity: [0, 1] }}
+					// 			transition={{
+					// 				duration: 0.25,
+					// 				ease: "easeInOut",
+					// 				staggerChildren: 0.5,
+					// 			}}
+					// 			className="app__work-hover app__flex"
+					// 		>
+					// 			{!work.tags.includes("Mobile") && (
+					// 				<a href={work.projectLink} target="_blank" rel="noreferrer">
+					// 					<motion.div
+					// 						whileInView={{ scale: [0, 1] }}
+					// 						whileHover={{ scale: [1, 0.9] }}
+					// 						transition={{ duration: 0.25 }}
+					// 						className="app__flex"
+					// 					>
+					// 						<AiFillEye />
+					// 					</motion.div>
+					// 				</a>
+					// 			)}
+
+					// 			<a href={work.codeLink} target="_blank" rel="noreferrer">
+					// 				<motion.div
+					// 					whileInView={{ scale: [0, 1] }}
+					// 					whileHover={{ scale: [1, 0.9] }}
+					// 					transition={{ duration: 0.25 }}
+					// 					className="app__flex"
+					// 				>
+					// 					<AiFillGithub />
+					// 				</motion.div>
+					// 			</a>
+					// 		</motion.div>
+					// 	</div>
+
+					// 	<div className="app__work-content app__flex">
+					// 		<h4 className="bold-text">{work.title}</h4>
+					// 		<p className="p-text" style={{ marginTop: 10 }}>
+					// 			{work.description}
+					// 		</p>
+
+					// 		<div className="app__work-tag app__flex">
+					// 			<p className="p-text">{work.tags[0]}</p>
+					// 		</div>
+					// 	</div>
+					// 	<div className="detailAction">
+					// 		<Link
+					// 			to={{ pathname: `/${work.title}`, state: { work: work } }}
+					// 			className="app__work-btn"
+					// 			state={{ work: work }}
+					// 		>
+					// 			More Details
+					// 		</Link>
+					// 		<Link
+					// 			to={{ pathname: `/${work.title}`, state: { work: work } }}
+					// 			state={{ work: work }}
+					// 		>
+					// 			<AiOutlineDoubleRight
+					// 				size={"1.5rem"}
+					// 				style={{ color: "#7980c9", marginTop: ".5rem" }}
+					// 			/>
+					// 		</Link>
+					// 	</div>
+					// </div>
 				))}
 			</motion.div>
 		</>
