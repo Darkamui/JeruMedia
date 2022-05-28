a116ce6d3b54aa3332551224a045041ecdb8d5d8
Daniel Jerusalmy <dannyerushalmi@gmail.com>
2022-05-28 02:28:53 -0400

    layout wrap update

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -8,11 +8,12 @@ const AppWrap = (Component, idName, classNames) =>
 				<SocialMedia />
 				<div className="app__wrapper app__flex">
 					<Component />
-
-					<div className="copyright">
-						<p className="p-text">@2022 JeruMedia</p>
-						<p className="p-text">All rights reserved</p>
-					</div>
+					{idName === "contact" && (
+						<div className="copyright">
+							<p className="p-text">@2022 JeruMedia</p>
+							<p className="p-text">All rights reserved</p>
+						</div>
+					)}
 				</div>
 				<NavigationDots active={idName} />
 			</div>
